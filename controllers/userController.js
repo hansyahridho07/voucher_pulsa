da2e9589c829ab1ed4d0671a587a17b8797393ff
Ilham Akbar <wordpalace72@gmail.com>
2021-01-28 21:56:58 +0700

    pairprojects

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,52 @@ class UserController{
   static showUser(req,res){
     res.render('user/home.ejs', {data: req.session.user, convertToRupiah})
   }
+
+  static edit(req, res) {
+    User.findAll()
+    .then((result) => {
+      res.render('user/editData', { data: result[0] });
+      console.log(result[0].name);
+    })
+    .catch((err) => {
+      res.send(err.message);
+    })
+  }
+
+  static editPost(req, res) {
+    const { name, phone_number, username, password, email } = req.body;
+    const updateUser = {
+      name,
+      phone_number,
+      username,
+      password,
+      email
+    }
+    User.update(updateUser, {
+      where: { }
+    })
+    .then(() => {
+      res.redirect('/users')
+    })
+    .catch((err) => {
+      res.send(err.message);
+    })
+  }
+
+  static topup(req, res) {
+    res.render('/user/topup')
+  }
+
+  static listVoucher(req, res) {
+    Voucher.findAll()
+    .then((data) => {
+      res.render('user/voucherList', { data })
+      // console.log(data);
+    })
+    .catch((err) => {
+      res.send(err.message);
+    })
+  }
 }
 
 module.exports = UserController
--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,9 @@ const UserController = require('../controllers/userController')
 
 router.get('/',authenticantion,UserController.showUser)
 
+router.get('/edit', UserController.edit);
+router.post('/edit', UserController.editPost);
+router.get('/transaction', UserController.topup);
+router.get('/vouchers', UserController.listVoucher);
+
 module.exports = router
